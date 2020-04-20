b06130c694197fa48afe3f8c7583c41e81eadf2c
asanchez412 <sanchezandres41997@gmail.com>
2020-04-20 18:46:03 -0300

    Agregar tests para ToYYYYMMDD

--- src/Program/ChangeFormat.cs
+++ src/Program/ChangeFormat.cs
@@ -1,4 +1,11 @@
 using System;
+// Escribir una función que recibe una cadena de caracteres como parámetro con una fecha de la forma
+// “dd/mm/aaaa” y devuelve la fecha en formato “aaaa­‐mm­‐dd”.
+// Ej.: 10/11/1977 -> 1977­‐11­‐10
+//
+// La función para cambiar el forma de la fecha está implementada en el método de clase ChangeFormat de la
+// clase Program. Ese método se invoca desde el método de clase Main de la clase Program que es el punto de
+// entrada del programa.
 
 namespace DateFormat
 {
@@ -6,7 +13,14 @@ namespace DateFormat
     {
         public static string ToYYYYMMDD(string year)
         {
-            return year.Substring(6) + "-" + year.Substring(3, 2) + "-" + year.Substring(0, 2);
+            if (year.Length == 10)
+            {
+                return year.Substring(6) + "-" + year.Substring(3, 2) + "-" + year.Substring(0, 2);
+            }
+            else
+            {
+                return "Fecha inválida.";
+            }
         }
     }
 }
---
+++ src/Test/Library.Test/Tests.cs
@@ -0,0 +1,55 @@
+using NUnit.Framework;
+using DateFormat;
+
+namespace Library.Test
+{
+
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase]
+        public void CantidadCaracteresCorrecta()
+        {
+            // en este ejemplo, el resultado es correcto pero no el ingreso
+            string testDate = "1270491997";
+            
+            // en este ejemplo, el resultado es correcto y también lo es el ingreso
+            string testDate2 = "12/04/1997";
+
+            Assert.AreEqual(ChangeFormat.ToYYYYMMDD(testDate),"1997-12-04");
+            Assert.AreEqual(ChangeFormat.ToYYYYMMDD(testDate2),"1997-12-04");
+        }
+        public void CantidadCaracteresIncorrecta()
+        {
+            string testDate = "1611561616161160";
+
+            Assert.AreEqual(ChangeFormat.ToYYYYMMDD(testDate),"Fecha inválida.");
+        }
+        [TestCase]
+        public void NoEsFecha()
+        {   
+            // el método devolverá una string modificada de cualquier string con length 10
+            // pero en el test debería de fallar siempre que no sea una fecha válida
+            string testDate = "12/04/199a";
+            string testDate2 = "12hfht1997";
+            Assert.AreEqual(ChangeFormat.ToYYYYMMDD(testDate),"199a-04-12");
+            Assert.AreEqual(ChangeFormat.ToYYYYMMDD(testDate2),"1997-fh-12.");
+        }
+        public void NoEsMes()
+        {
+            // el método devolverá una string modificada de cualquier string con length 10
+            // pero en el test debería de fallar siempre que no sea una fecha válida
+            string testDate = "12/98/1997";
+
+            Assert.AreEqual(ChangeFormat.ToYYYYMMDD(testDate),"1997-98-12");
+        }
+        public void NoEsDia()
+        {
+            // el método devolverá una string modificada de cualquier string con length 10
+            // pero en el test debería de fallar siempre que no sea una fecha válida
+            string testDate = "54/04/1997";
+
+            Assert.AreEqual(ChangeFormat.ToYYYYMMDD(testDate),"1997-04-54");
+        }
+    }
+}
